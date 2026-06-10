7f40a7a2feda38aeb81f028f411bead662f28df6
Kisaragi Hiu <mail@kisaragi-hiu.com>
2026-06-10 14:40:42 +0900

    maybe run it

--- collect-and-train.sh
+++ collect-and-train.sh
@@ -2,10 +2,13 @@
 set -euo pipefail
 set -x
 
-node extract.ts >data/ftg/ftg.training_text
-cat ~/git/kisaragi-rime-taigi/taigi-poj.syllables.dict.yaml | sed '/[:\.#-]/d;s/\t.*//' >>data/ftg/ftg.training_text
-bunx @kemdict/kesi --to poj --input data/ftg/ftg.training_text --output data/ftg/ftg.training_text.poj
-bunx @kemdict/kesi --to kip --input data/ftg/ftg.training_text --output data/ftg/ftg.training_text.kip
-cat data/ftg/ftg.training_text.poj data/ftg/ftg.training_text.kip >data/ftg/ftg.training_text
-make training MODEL_NAME=ftg START_MODEL=eng
+if [ ! -f "data/ftg/ftg.training_text" ]; then
+    node extract.ts >data/ftg/ftg.training_text
+    cat ~/git/kisaragi-rime-taigi/taigi-poj.syllables.dict.yaml | sed '/[:\.#-]/d;s/\t.*//' >>data/ftg/ftg.training_text
+    bunx @kemdict/kesi --to poj --input data/ftg/ftg.training_text --output data/ftg/ftg.training_text.poj
+    bunx @kemdict/kesi --to kip --input data/ftg/ftg.training_text --output data/ftg/ftg.training_text.kip
+    cat data/ftg/ftg.training_text.poj data/ftg/ftg.training_text.kip >data/ftg/ftg.training_text
+fi
+make TESSDATA="data/tessdata" data/tessdata/eng.traineddata
+make training MODEL_NAME=ftg START_MODEL=eng TESSDATA="data/tessdata"
 make traineddata MODEL_NAME=ftg
