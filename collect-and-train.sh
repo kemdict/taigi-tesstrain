09fe4cff3c568a0d1adffcbb7a682ccafa458e99
Kisaragi Hiu <mail@kisaragi-hiu.com>
2026-06-18 14:40:35 +0900

    Revert to before using pytesstrain to create line-by-line gt texts
    This reverts these commits:
      fdc9808 Revert "-j 200% since it still doesn't max out CPU use"
      e566f70 -j 200% since it still doesn't max out CPU use
      9b6ac0e Separate box file and lstmf file generation
      c9aa5ca progress info
      d471469 Fix incorrect variable reference from copy pasting from Make
      eafdca1 Generate lstmf files in parallel using Parallel instead of Make
      b7c4f75 return if ground truth files are already made
      cfd8d6b Use another font instead of Roboto Condensed
      78ac65e Use pytesstrain to create line-by-line gt texts
    
    After training it produces a model that seemingly does the same thing as the
    English base model. Somehow its generation process does not work. If we want to
    split line-by-line like pytesstrain's create_ground_truth, we can just take its
    logic:
    
        # in pytesstrain's cli/create_ground_truth.py
        for line_number, line in enumerate(RE_LINE_ENDING.split(source.read_text(encoding='utf8')), 1):
            tidy_line = line.rstrip()

--- collect-and-train.sh
+++ collect-and-train.sh
@@ -29,13 +29,13 @@ make_text() {
     if [ ! -f "$TRAINING_TEXT_DIR"/ftg.training_text.all.txt ]; then
         mkdir -p "$TRAINING_TEXT_DIR"
         echo Extracting training text...
-        node extract.ts --bucket-size 0 "$TRAINING_TEXT_DIR"
+        node extract.ts --bucket-size 10 "$TRAINING_TEXT_DIR"
         echo Copying syllables...
         cat yataigi-poj.syllables.dict.yaml |
-            sed '/[:\.#-]/d;s/\t.*//' >>"$TRAINING_TEXT_DIR"/ftg.training_text.all.poj
+            sed '/[:\.#-]/d;s/\t.*//' >"$TRAINING_TEXT_DIR"/ftg.training_text.syllables.poj
         echo Converting POJ to KIP...
         parallel bunx @kemdict/kesi --to kip --input "{}" --output "{.}".kip ::: "$TRAINING_TEXT_DIR"/*.poj
-        echo Combining POJ and KIP...
+        echo Combining POJ and KIP samples...
         find "$TRAINING_TEXT_DIR" -type f -path "*.poj" | while read -r f; do
             cat "$f" "${f%.*}".kip >"${f%.*}".txt
         done
@@ -45,53 +45,82 @@ make_text() {
     fi
 }
 
+make_one_lstmf() {
+    local f="$1"
+    local short
+    short="$(basename "$f" .txt | sed s/training_text.//)"
+    if [ "$short" == ftg.all ]; then
+        return
+    fi
+    mkdir -p data/ftg-parts/
+    uv run python src/tesstrain --linedata_only \
+        --lang ftg \
+        --langdata_dir data/langdata \
+        --tessdata_dir /usr/share/tessdata \
+        --training_text "$f" \
+        --output_dir data/ftg-parts/"$short"
+}
+export -f make_one_lstmf
 
-make_gt() {
-    if [ -f "$OUTPUT_DIR/all-gt" ]; then return; fi
-    echo Generating ground truth files from input text...
-    mkdir -p "$GT_DIR" "$OUTPUT_DIR"
-    uv run create_ground_truth -f "Charis,Dejavu Serif Italic,Dejavu Serif,Iosevka,Liberation Serif,Noto Sans,Noto Sans CJK TC,Noto Serif,Fira Sans Compressed Ultra-Condensed" \
-        "$TRAINING_TEXT_DIR"/ftg.training_text.all.txt \
-        "$GT_DIR"
-    echo Writing OUTPUT_DIR/all-gt...
-    cp "$TRAINING_TEXT_DIR"/ftg.training_text.all.txt "$OUTPUT_DIR"/all-gt
+make_full_lstmf() {
+    # Generate the main thing first for more complete unicharset etc.
+    # This takes at least 24 hours!
+    # We need more than just the trainedmodel files from tessdata.
+    # /usr/share/tessdata works for this.
+    uv run python src/tesstrain --linedata_only \
+        --lang ftg \
+        --langdata_dir data/langdata \
+        --tessdata_dir /usr/share/tessdata \
+        --training_text "$TRAINING_TEXT_DIR"/ftg.training_text.all.txt \
+        --output_dir "$GT_DIR"
 }
 
-make_one_box() {
-    local image="$1"
-    local noext="$2"
-    if [ -f "$noext".box ]; then return; fi
-    # Copies of variables from the Makefile
-    # The Makefile logic for this variable is to use generate_wordstr_box.py if
-    # LANG_TYPE is Indic or RTL, and generate_line_box.py otherwise (default).
-    GENERATE_BOX_SCRIPT=generate_line_box.py
-    PYTHONIOENCODING=utf-8 uv run python "$GENERATE_BOX_SCRIPT" -i "$image" -t "$noext".gt.txt > "$noext".box
-}; export -f make_one_box
+make_split_lstmf() {
+    echo Generating lstmf files from input text...
+    if [ -d "$GT_DIR" ] && [ -d "$OUTPUT_DIR" ]; then
+        echo "$GT_DIR" and "$OUTPUT_DIR" already present, assuming split lstmf files are already made
+        return
+    fi
+    # Generate the lstmf files for each segments
+    if [ ! -d data/ftg-parts ]; then
+        # FIXME some of these calls may be failing?
+        find "$TRAINING_TEXT_DIR" -type f -path "*.txt" -print0 |
+            parallel -0 --eta make_one_lstmf || true
+    fi
+    echo Moving generated lstmf files to the right place...
+    set -x
+    # mkdir -p "$GT_DIR"
 
-make_one_lstmf() {
-    local image="$1"
-    local noext="$2"
-    if [ -f "$noext".lstmf ]; then return; fi
-    # Copies of variables from the Makefile
-    PSM=13
-    tesseract "$image" "$noext" --psm "$PSM" lstm.train
-}; export -f make_one_lstmf
+    # They all have the same basename, so add their directory names onto the
+    # final name to avoid overwriting them.
+    # And just in case, also use mv -n to not overwrite anything.
+    # Note about the {= ... =} magic:
+    # - {= ... =} in Parallel means replace input items with the result of
+    #   running the Perl expression in between
+    # - a sed-style s/from/to/ expression is a valid Perl expression that works
+    #   for this purpose
+    # - We are replacing "^.*\/([^\/]+)\/([^\/]*)" with "\1-\2", to turn
+    #   "path/to/ftg.100/foo.lstmf" into "ftg.100-foo.lstmf".
+    #   (slashes are escaped because a bare slash is part of the s/from/to/ syntax)
+    #   In Elisp rx syntax this would be:
+    #   (rx bol (* any) "/"
+    #       (group (+ (not "/"))) "/"
+    #       (group (* (not "/"))))
+    mkdir -p "$GT_DIR" "$OUTPUT_DIR"
+    find data/ftg-parts -path "*.lstmf" -print0 |
+        parallel -0 mv -n '{}' "$GT_DIR"/'{= s/^.*\/([^\/]+)\/([^\/]*)/\1-\2/ =}'
+    echo Writing OUTPUT_DIR/all-gt...
+    cat "$TRAINING_TEXT_DIR"/ftg.training_text.all.txt \
+        "$TRAINING_TEXT_DIR"/ftg.training_text.syllables.txt \
+        >"$OUTPUT_DIR"/all-gt
+}
 
-make_lstmf() {
-    # We do this ourselves to use parallel instead of Make for this step.
-    # generate lstmf files
-    echo Generating box files...
-    find "$GT_DIR" -path "*.tif" -print0 |
-        parallel --eta -0 make_one_box "{}" "{.}" || true
-    echo Generating lstmf files...
-    find "$GT_DIR" -path "*.tif" -print0 |
-        parallel --eta -0 make_one_lstmf "{}" "{.}" || true
-    echo Writing all-lstmf...
-    find "$GT_DIR" -path "*.lstmf" > "$OUTPUT_DIR"/all-lstmf
-    # Copied from Makefile
-    RANDOM_SEED=0
-    echo Shuffling all-lstmf...
-    uv run python shuffle.py "$RANDOM_SEED" "$OUTPUT_DIR"/all-lstmf
+merge_our_unicharsets() {
+    # We can probably also just rely on the Makefile instead, which takes
+    # ALL_GT, runs unicharset_extractor on it, then merges it with the
+    # unicharset of the START_MODEL.
+    readarray files < <(find data/ftg-parts -path "*.unicharset")
+    merge_unicharsets "${files[@]}" "$OUTPUT_DIR"/unicharset
 }
 
 train() {
@@ -101,7 +130,6 @@ train() {
         awk '{ print $2 "\t" $1 }' |
         sort -rn |
         awk '{ print $2 }' >"$OUTPUT_DIR"/ftg.wordlist
-
     make training MODEL_NAME=ftg START_MODEL=eng TESSDATA="data/tessdata"
     mv data/ftg.traineddata data/ftg-best.traineddata
     # Also generate the "fast" model (I think this is called quantization nowadays)
@@ -117,6 +145,5 @@ train() {
 
 download_data
 make_text
-make_gt
-make_lstmf
+make_split_lstmf
 train
