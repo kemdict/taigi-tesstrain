d880f130e679d2ecb2d96c70418f40c2b0c257eb
Kisaragi Hiu <mail@kisaragi-hiu.com>
2026-06-21 22:44:08 +0900

    the variable is still not defined...

--- collect-and-train.sh
+++ collect-and-train.sh
@@ -101,7 +101,7 @@ export -f make_one_lstmf
 make_one_lstmf_from_gt() {
     local input="$1"
     local noext="$2"
-    PYTHONIOENCODING=utf-8 python "$GENERATE_BOX_SCRIPT" -i "$input" -t "$noext".gt.txt >"$noext".box
+    PYTHONIOENCODING=utf-8 python generate_line_box.py -i "$input" -t "$noext".gt.txt >"$noext".box
     # Page segmentation mode, as defined in the Makefile.
     PSM=13
     tesseract "$1" "$2" --psm raw_line lstm.train
